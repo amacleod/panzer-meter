2d20ecb921ba504e2970ae855cc959684801d29f
Allister MacLeod <allister.macleod@gmail.com>
2021-03-19 21:48:28 -0400

    But icanhazip does not emit JSON

--- app/panzer.py
+++ app/panzer.py
@@ -7,4 +7,4 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     response = requests.get("https://icanhazip.com")
-    return "Hello, World! IP for request was '{0}'.".format(response.json()['origin'])
+    return "Hello, World! IP for request was '{0}'.".format(response.text)
